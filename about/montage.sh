af1b48b2a09bb6cb7d8d01f05458765f58486718
Anthony Wang <ta180m@pm.me>
2021-07-28 12:13:04 -0500

    Add slightly larger image

--- about/montage.sh
+++ about/montage.sh
@@ -5,5 +5,5 @@ for old in *; do
     rsvg-convert "$old" -h 1080 -f svg -o "$new"
 done
 cd ..
-montage icons/*-large.svg -geometry 480x480 -tile 8x4 -border 20 -bordercolor white montage.png
+montage icons/*-large.svg -geometry 540x540 -tile 8x4 -border 20 -bordercolor white montage.png
 gio trash icons/*-large.svg
